2d20dfa3acdcf74badc25a4763c2820be54096d3
serega2593 <serega2593@gmail.com>
2014-02-05 22:16:24 +0200

    add AsyncTask

--- src/com/example/one/MainActivity.java
+++ src/com/example/one/MainActivity.java
@@ -1,35 +1,60 @@
 package com.example.one;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.ListActivity;
-import android.view.Menu;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.*;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
+
+    ListView listView;
+    ProgressBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        MyAdapter adapter = new MyAdapter(this,generateData());
-
-        ListView listview = (ListView) findViewById(R.id.list);
-        listview.setAdapter(adapter);
-
+        listView = (ListView) findViewById(R.id.list);
+        bar = (ProgressBar) findViewById(R.id.progressBar);
+        new DelayOutput().execute();
     }
 
-    private ArrayList<Data> generateData() {
+    public static ArrayList<Data> generateData() {
         ArrayList<Data> dataArrayList = new ArrayList<Data>();
         dataArrayList.add(new Data(R.drawable.d1,"Knopka1"));
         dataArrayList.add(new Data(R.drawable.d2,"Knopka2"));
 
         return dataArrayList;
     }
+
+    class DelayOutput extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            bar.setVisibility(View.GONE);
+            MyAdapter adapter = new MyAdapter(MainActivity.this,generateData());
+            listView.setAdapter(adapter);
+        }
+    }
 }
+
